263b7cf1f8ada2449c3b93d8e098408d5684dc94
Kosuke Inoue <mmts0106@gmail.com>
2019-02-05 12:20:12 +0900

    Modified for coverage path

--- carrot_planner/src/carrot_planner.cpp
+++ carrot_planner/src/carrot_planner.cpp
@@ -1,4 +1,4 @@
-/*********************************************************************
+﻿/*********************************************************************
 *
 * Software License Agreement (BSD License)
 *
@@ -35,10 +35,70 @@
 * Authors: Eitan Marder-Eppstein, Sachin Chitta
 *********************************************************************/
 #include <carrot_planner/carrot_planner.h>
+#include "ros/ros.h"
 #include <pluginlib/class_list_macros.h>
 #include <tf2/convert.h>
 #include <tf2/utils.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf/transform_listener.h>
+#include <unistd.h>
+#include <math.h>
+
+geometry_msgs::PoseStamped prev_goal;
+geometry_msgs::PoseStamped current_pose;
+geometry_msgs::PoseStamped new_goal;
+ros::NodeHandle nh("point_pub");
+ros::Publisher point_pub = nh.advertise<geometry_msgs::PoseStamped>("point_on_path", 10);
+
+//頂点の定義
+struct Vertex2D{
+        double x;
+        double y;
+};
+//ベクトルの定義(頂点と同じ)
+#define Vector2D Vertex2D
+
+//単位ベクトル生成
+Vector2D ceate_unit_vector( Vector2D v )
+{
+        double len = pow( ( v.x * v.x ) + ( v.y * v.y ), 0.5 );//ベクトル長さ
+
+        Vector2D ret;
+        ret.x = v.x / len;
+        ret.y = v.y / len;
+
+        return ret;
+}
+
+//ベクトル内積
+double dot_product(Vector2D vl, Vector2D vr) {
+        return vl.x * vr.x + vl.y * vr.y;
+}
+
+//点Pと直線ABから線上最近点を求める
+Vector2D NearPosOnLine(Vertex2D P, Vertex2D A, Vertex2D B )
+{
+        Vector2D AB,AP;//ベクトルAB AP
+
+        AB.x = B.x - A.x;
+        AB.y = B.y - A.y;
+        AP.x = P.x - A.x;
+        AP.y = P.y - A.y;
+
+        //ABの単位ベクトルを計算
+        Vector2D nAB = ceate_unit_vector(AB);
+
+        //Aから線上最近点までの距離（ABベクトルの後ろにあるときはマイナス値）
+        double dist_AX = dot_product( nAB, AP );
+
+        //線上最近点
+        Vector2D ret;
+        ret.x = A.x + ( nAB.x * dist_AX );
+        ret.y = A.y + ( nAB.y * dist_AX );
+
+        return ret;
+}
+
 
 //register this planner as a BaseGlobalPlanner plugin
 PLUGINLIB_EXPORT_CLASS(carrot_planner::CarrotPlanner, nav_core::BaseGlobalPlanner)
@@ -63,6 +123,27 @@ namespace carrot_planner {
       private_nh.param("min_dist_from_robot", min_dist_from_robot_, 0.10);
       world_model_ = new base_local_planner::CostmapModel(*costmap_); 
 
+      
+      tf::TransformListener listener;
+      tf::StampedTransform transform;
+      try{
+          listener.waitForTransform("map", "base_link", ros::Time(0), ros::Duration(10.0));
+          listener.lookupTransform("map", "base_link", ros::Time(0), transform);
+      }
+      catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        ros::Duration(1.0).sleep();
+      }
+
+      prev_goal.header.frame_id = "map";
+      prev_goal.pose.position.x = transform.getOrigin().x();
+      prev_goal.pose.position.y = transform.getOrigin().y();
+      prev_goal.pose.position.z = transform.getOrigin().z();
+      prev_goal.pose.orientation.x = transform.getRotation().x();
+      prev_goal.pose.orientation.y = transform.getRotation().y();
+      prev_goal.pose.orientation.z = transform.getRotation().z();
+      prev_goal.pose.orientation.w = transform.getRotation().w();
+      
       initialized_ = true;
     }
     else
@@ -77,7 +158,7 @@ namespace carrot_planner {
     }
 
     std::vector<geometry_msgs::Point> footprint = costmap_ros_->getRobotFootprint();
-    //if we have no footprint... do nothing
+
     if(footprint.size() < 3)
       return -1.0;
 
@@ -86,9 +167,9 @@ namespace carrot_planner {
     return footprint_cost;
   }
 
-
   bool CarrotPlanner::makePlan(const geometry_msgs::PoseStamped& start, 
       const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan){
+//    ros::Subscriber sub = nh.subscribe("/move_base/current_goal", 100, goal_callback);
 
     if(!initialized_){
       ROS_ERROR("The planner has not been initialized, please call initialize() to use the planner");
@@ -148,22 +229,51 @@ namespace carrot_planner {
       }
       scale -=dScale;
     }
+      
+      //if we have no footprint... do nothing
+    if(goal.header.stamp != new_goal.header.stamp){
+        prev_goal.pose.position.x = new_goal.pose.position.x;
+        prev_goal.pose.position.y = new_goal.pose.position.y;
+        point_pub.publish(prev_goal);
+}
+      
+      new_goal = goal;
+      tf2::Quaternion goal_quat;
+      goal_quat.setRPY(0, 0, target_yaw);
 
-    plan.push_back(start);
-    geometry_msgs::PoseStamped new_goal = goal;
-    tf2::Quaternion goal_quat;
-    goal_quat.setRPY(0, 0, target_yaw);
+      current_pose = start;
 
-    new_goal.pose.position.x = target_x;
-    new_goal.pose.position.y = target_y;
+      Vertex2D start_vector;
+      start_vector.x = prev_goal.pose.position.x;
+      start_vector.y = prev_goal.pose.position.y;
+      Vertex2D goal_vector;
+      goal_vector.x = new_goal.pose.position.x;
+      goal_vector.y = new_goal.pose.position.y;
+      Vertex2D corrent_position;
+      corrent_position.x = current_pose.pose.position.x;
+      corrent_position.y = current_pose.pose.position.y;
 
-    new_goal.pose.orientation.x = goal_quat.x();
-    new_goal.pose.orientation.y = goal_quat.y();
-    new_goal.pose.orientation.z = goal_quat.z();
-    new_goal.pose.orientation.w = goal_quat.w();
+      new_goal.pose.position.x = target_x;
+      new_goal.pose.position.y = target_y;
 
+      new_goal.pose.orientation.x = goal_quat.x();
+      new_goal.pose.orientation.y = goal_quat.y();
+      new_goal.pose.orientation.z = goal_quat.z();
+      new_goal.pose.orientation.w = goal_quat.w(); 
+    
+      Vertex2D point_on_line;
+      point_on_line = NearPosOnLine(corrent_position, start_vector, goal_vector);
+    int divided_number = 50;
+    for(int i=0;i<divided_number;i++){
+    geometry_msgs::PoseStamped mid_goal;
+    mid_goal.header.frame_id = "map";
+    mid_goal.header.stamp = current_pose.header.stamp;
+    mid_goal.pose.position.x = point_on_line.x + (new_goal.pose.position.x-point_on_line.x)/divided_number * i;
+    mid_goal.pose.position.y = point_on_line.y + (new_goal.pose.position.y-point_on_line.y)/divided_number * i;
+    mid_goal.pose.orientation = new_goal.pose.orientation; 
+    plan.push_back(mid_goal);
+    }
     plan.push_back(new_goal);
     return (done);
-  }
-
+}
 };
